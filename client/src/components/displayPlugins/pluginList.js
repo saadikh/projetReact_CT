cf08c23a5b3af36f143e5f9bddd099e0b64dbab5
chendatao407786 <datao.chen@etu.unice.fr>
2018-12-17 00:55:21 +0100

    Update pluginList.js

--- client/src/components/displayPlugins/pluginList.js
+++ client/src/components/displayPlugins/pluginList.js
@@ -1,38 +1,37 @@
 import React, { Component } from 'react';
 import PluginItem from './pluginItem';
 import { connect } from 'react-redux';
-import {getPlugins,getPluginsByTag} from '../../actions/pluginActions';
+import { getPlugins } from '../../actions/pluginActions';
 import PropTypes from 'prop-types';
 require('./css/index.css');
 class PluginList extends Component {
     componentWillMount() {
         this.props.getPlugins();
     }
-    
+
     render() {
         let listPlugins = this.props.plugin.plugins.map((element, index) => {
-            return <PluginItem isEmpty="false" plugin={element} key={index}></PluginItem> 
+            return <PluginItem isEmpty="false" plugin={element} key={index}></PluginItem>
         })
-        
-        return (
 
-            <div className="body">
-                <div className="titres">
-                    <h2>Inspiration in all the Classics</h2>
-                    <span className="sub-description">All the famous stompboxes, FX, synths, sequencers and amps that made history</span>
+            return (
+                <div className="body">
+                    <div className="titres">
+                        <h2>Inspiration in all the Classics</h2>
+                        <span className="sub-description">All the famous stompboxes, FX, synths, sequencers and amps that made history</span>
+                    </div>
+                    <div className="listPlugins">
+                        {listPlugins}
+                        <PluginItem isEmpty="true"></PluginItem>
+                        <PluginItem isEmpty="true"></PluginItem>
+                        aaa
+                    </div>
                 </div>
-                <div className="listPlugins">
-                    {listPlugins}
-                    <PluginItem isEmpty="true"></PluginItem>
-                    <PluginItem isEmpty="true"></PluginItem>
-                </div>
-            </div>
-        )
+            )
     }
 }
 PluginList.propTypes = {
     getPlugins: PropTypes.func.isRequired,
-    getPluginsByTag: PropTypes.func.isRequired,
     plugin: PropTypes.object.isRequired
 }
 
@@ -40,4 +39,4 @@ const mapStateToProps = (state) => ({
     plugin: state.plugin
 })
 
-export default connect(mapStateToProps,{getPlugins,getPluginsByTag})(PluginList);
+export default connect(mapStateToProps, { getPlugins })(PluginList);

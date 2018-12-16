eff37da1fd34473687201e39e45efab5a7e2ba0d
chendatao407786 <datao.chen@etu.unice.fr>
2018-12-17 00:55:57 +0100

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import{combineReducers} from 'redux';
 import pluginReducer from './pluginReducer';
 import flashMessage from './flashMessage';
 import authReducer from './authReducer';
+import userDataReducer from './userDataReducer';
 
 export default combineReducers({
     plugin: pluginReducer,
     flashMessage: flashMessage,
-    auth: authReducer
+    auth: authReducer,
+    userData: userDataReducer
 });
